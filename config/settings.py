1b4c3f7d1cead43c7cc7bd24905f3a87a031e50a
OmgbaKevin <kevinomgba1@gmail.com>
2026-08-24 14:05:09 +0100

    static

--- config/settings.py
+++ config/settings.py
@@ -152,6 +152,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Ensure STATIC_ROOT uses an absolute path like this:
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
